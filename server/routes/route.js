895328d7e91cbfa01ef533a422a9634f1d970d96
Rahul Ohol <rahulohol01@gmail.com>
2023-07-30 23:15:14 +0530

    api's done

---
+++ server/routes/route.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+  getProductById,
+  getProducts,
+} from "../controller/product-controller.js";
+import { userSignUp, userLogIn } from "../controller/user-controller.js";
+
+const router = express.Router();
+
+//login & signup
+router.post("/signup", userSignUp);
+router.post("/login", userLogIn);
+
+//products
+router.get("/products", getProducts);
+router.get("/product/:id", getProductById);
+
+export default router;
